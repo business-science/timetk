2d951a38cc422208b868f9beee9b22fa19a16f72
Matt Dancho <mdancho@gmail.com>
2020-04-11 20:44:10 -0400

    add taylor dataset

---
+++ R/data-taylor_30_min.R
@@ -0,0 +1,20 @@
+#' Half-hourly electricity demand
+#'
+#' Half-hourly electricity demand in England and Wales from Monday 5 June 2000
+#' to Sunday 27 August 2000. Discussed in Taylor (2003).
+#'
+#' @format A tibble: 4,032 x 2
+#' - `date`: A date-time variable in 30-minute increments
+#' - `value`: Electricity demand in Megawatts
+#'
+#' @references Taylor, J.W. (2003) Short-term electricity demand forecasting
+#' using double seasonal exponential smoothing. \emph{Journal of the
+#' Operational Research Society}, \bold{54}, 799-805.
+#'
+#' @source James W Taylor
+#'
+#' @examples
+#' taylor_30_min
+#'
+"taylor_30_min"
+
