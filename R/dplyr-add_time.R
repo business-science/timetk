21c2ead2caddf8b6da21a8ee495785cd216d03e1
Matt Dancho <mdancho@gmail.com>
2020-04-13 14:31:59 -0400

    add_time: initial commit

---
+++ R/dplyr-add_time.R
@@ -0,0 +1,201 @@
+#' Add (For Time Series): Add a period to a time series
+#'
+#' The easiest way to add / subtract a period to a time series date or date-time vector.
+#'
+#' @param index A date or date-time vector. Can also accept a character representation.
+#' @param period A period to add. Accepts character strings like "5 seconds", "2 days",
+#'  and complex strings like "1 month 4 days 34 minutes".
+#'
+#' @return A `date` or datetime (`POSIXct`) vector the same length as `index` with the
+#' time values shifted +/- a period.
+#'
+#' @details
+#'
+#' A convenient wrapper for `lubridate::period()`. Adds and subtracts a period from a
+#' time-based index. Great for:
+#'
+#' - Finding a timestamp n-periods into the future or past
+#' - Shifting a time-based index. Note that `NA` values may be present where dates don't exist.
+#'
+#' __Period Specification__
+#'
+#' The `period` argument accepts complex strings like:
+#'
+#' - "1 month 4 days 43 minutes"
+#' - "second = 3, minute = 1, hour = 2, day = 13, week = 1"
+#'
+#' @seealso
+#'
+#' Other Time-Based vector functions:
+#'
+#' - [between_time()] - Range detection for date or date-time sequences.
+#'
+#' Underlying function:
+#'
+#' - [lubridate::period()]
+#'
+#'
+#' @examples
+#' library(timetk)
+#'
+#'
+#' # ---- LOCATING A DATE N-PERIODS IN FUTURE / PAST ----
+#'
+#' # Forward
+#' "2021-01-01 00:00:00" %+time% "1 hour 34 seconds"
+#'
+#' # Backward
+#' "2021-01-01 00:00:00" %-time% "1 hour 34 seconds"
+#'
+#' # ---- INDEX SHIFTING ----
+#'
+#' index_daily <- tk_make_timeseries("2016-01-01", "2016-02-01", by = "day")
+#'
+#' # ADD TIME
+#' # - Note `NA` values created where a daily dates aren't possible
+#' #   (e.g. Feb 29 & 30, 2016 doesn't exist).
+#' add_time(index_daily, "1 month")
+#'
+#' # Using Infix `%+time%`
+#' index_daily %+time% "1 month"
+#'
+#' # Subtracting Time (These do the same thing)
+#' index_daily %+time% "-1 month"
+#' index_daily %-time% "1 month"
+#'
+#'
+#'
+#'
+#' @name add_time
+#' @export
+
+# ADD ----
+
+#' @export
+#' @rdname add_time
+add_time <- function(index, period) {
+    UseMethod("add_time", index)
+}
+
+#' @export
+add_time.character <- function(index, period) {
+    index <- readr::parse_guess(index)
+    add_time(index, period)
+}
+
+#' @export
+add_time.POSIXt <- function(index, period) {
+    time_adder(index, period)
+}
+
+#' @export
+add_time.Date <- function(index, period) {
+    time_adder(index, period)
+}
+
+#' @export
+add_time.yearmon <- function(index, period) {
+    index <- lubridate::as_date(index)
+    message("Converting to date class")
+    time_adder(index, period)
+}
+
+#' @export
+add_time.yearqtr <- function(index, period) {
+    index <- lubridate::as_date(index)
+    message("Converting to date class")
+    time_adder(index, period)
+}
+
+#' @export
+add_time.numeric <- function(index, period) {
+    stop("Index must be a non-numeric time-based class.")
+}
+
+#' @export
+add_time.default <- function(index, period) {
+    rlang::abort(paste0("No method for class ", class(index)[[1]], "."))
+}
+
+# SUBTRACT ----
+
+#' @export
+#' @rdname add_time
+subtract_time <- function(index, period) {
+    UseMethod("subtract_time", index)
+}
+
+#' @export
+subtract_time.character <- function(index, period) {
+    index <- readr::parse_guess(index)
+    subtract_time(index, period)
+}
+
+#' @export
+subtract_time.POSIXt <- function(index, period) {
+    time_subtracter(index, period)
+}
+
+#' @export
+subtract_time.Date <- function(index, period) {
+    time_subtracter(index, period)
+}
+
+#' @export
+subtract_time.yearmon <- function(index, period) {
+    index <- lubridate::as_date(index)
+    message("Converting to date class")
+    time_subtracter(index, period)
+}
+
+#' @export
+subtract_time.yearqtr <- function(index, period) {
+    index <- lubridate::as_date(index)
+    message("Converting to date class")
+    time_subtracter(index, period)
+}
+
+#' @export
+subtract_time.numeric <- function(index, period) {
+    stop("Index must be a non-numeric time-based class.")
+}
+
+#' @export
+subtract_time.default <- function(index, period) {
+    rlang::abort(paste0("No method for class ", class(index)[[1]], "."))
+}
+
+
+#' @export
+#' @rdname add_time
+`%+time%` <- function(index, period) {
+    add_time(index, period)
+}
+
+#' @export
+#' @rdname add_time
+`%-time%` <- function(index, period) {
+    subtract_time(index, period)
+}
+
+# Utils ----
+
+time_adder <- function(index, period) {
+    check_quarter(period)
+    index + lubridate::period(period)
+}
+
+time_subtracter <- function(index, period) {
+    check_quarter(period)
+    index - lubridate::period(period)
+}
+
+check_quarter <- function(period) {
+    if (tolower(period) %>% stringr::str_detect("quarter")) {
+        rlang::abort("`quarter` detected. Try using `3 month` increments instead.")
+    }
+}
+
+
+
+
---
+++ R/utils-parse_period.R
@@ -0,0 +1,128 @@
+#### Utils ---------------------------------------------------------------------
+
+# Utils ----
+parse_period <- function(period) {
+    UseMethod("parse_period")
+}
+
+#' @export
+parse_period.default <- function(period) {
+    rlang::abort("Unsupported period specification. Only characters are allowed.")
+}
+
+#' @export
+parse_period.character <- function(period) {
+
+    # Cannot supply vector of periods. 1 character only
+    if(length(period) != 1) {
+        rlang::abort("Only 1 period can be specified.")
+    }
+
+    # Split on " "
+    period_split <- unlist(strsplit(period, " "))
+
+    # Assign period_freq / period_char
+    if(length(period_split) == 1) {
+
+        period_freq <- 1
+        period_char <- period_split
+
+    } else if(length(period_split) == 2) {
+
+        assert_freq_coerce_to_numeric(period_split[1])
+        period_freq <- as.numeric(period_split[1])
+        period_char <- period_split[2]
+
+    } else {
+        rlang::abort("A maximum of 1 space character is allowed in the period.")
+    }
+
+    period_char <- parse_period_char(period_char)
+
+    period_list <- list(freq = period_freq, period = period_char)
+
+    period_list <- check_subsecond_period(period_list)
+
+    period_list
+}
+
+# Check that the RHS of period is correct
+parse_period_char <- function(period) {
+
+    if(string_length(period) == 1) {
+        p <- parse_letter_period(period)
+    } else {
+        p <- parse_word_period(period)
+    }
+
+    p
+}
+
+# >1 letter character parsing
+parse_word_period <- function(period) {
+
+    key <- c("year", "quarter", "month", "week",
+             "da",   "hour",    "min",   "sec",
+             "ms",   "mil",     "us",    "mic")
+
+    value <- c("year",     "quarter",  "month",    "week",
+               "day",      "hour",     "min",      "sec",
+               "millisec", "millisec", "microsec", "microsec")
+
+    loc_vec <- pmatch(key, period)
+    parsed_period <- value[!is.na(loc_vec)]
+
+    if(length(parsed_period) == 0) {
+        rlang::abort(stringr::str_glue("Period '{period}' specified incorrectly."))
+    }
+
+    parsed_period
+}
+
+# 1 letter parsing, case sensitive
+parse_letter_period <- function(period) {
+    switch (period,
+            "y" = "year",     "Y" = "year",
+            "q" = "quarter",  "Q" = "quarter",
+            "m" = "month",     # Case sensitive
+            "w" = "week",     "W" = "week",
+            "d" = "day",      "D" = "day",
+            "h" = "hour",     "H" = "hour",
+            "M" = "min",       # Case sensitive
+            "s" = "sec",      "S" = "sec",
+            "l" = "millisec", "L" = "millisec",
+            "u" = "microsec", "U" = "microsec",
+            rlang::abort(stringr::str_glue("Period '{period}' specified incorrectly."))
+    )
+}
+
+# Check that the freq can be coerced to numeric
+assert_freq_coerce_to_numeric <- function(freq) {
+    assertthat::assert_that(
+        # Coercing to numeric should give a number, not NA
+        suppressWarnings(!is.na(as.numeric(freq))),
+        msg = "Frequency must be coercible to numeric."
+    )
+}
+
+# If sub-second resolution, change to correct second representation
+check_subsecond_period <- function(period_list) {
+
+    multiplier <- switch(period_list$period,
+                         "millisec" = 1000,
+                         "microsec" = 1000000,
+                         0 # Default for >subsecond periods so it returns
+    )
+
+    if(!multiplier) return(period_list)
+
+    period_list$freq   <- period_list$freq / multiplier
+    period_list$period <- "sec"
+
+    period_list
+}
+
+string_length <- function(x) {
+    split <- unlist(strsplit(x, ""))
+    length(split)
+}
