c909d5b8badee6cf208336eeae29f59956bfcf52
Matt Dancho <mdancho@gmail.com>
2020-04-13 16:36:51 -0400

    tk_make_timeseries: length_out can be time-based phrase

--- R/make-tk_make_timeseries.R
+++ R/make-tk_make_timeseries.R
@@ -12,7 +12,9 @@
 #'  `"sec"`, `"min"`, `"hour"`, `"day"`, `"week"`, `"month"`, `"quarter"` or `"year"`.
 #'  You can create regularly spaced sequences using phrases like `by = "10 min"`. See Details.
 #' @param length_out Optional length of the sequence. Can be used instead of one of:
-#'  `start_date`, `end_date`, or `by`.
+#'  `start_date`, `end_date`, or `by`. Can be specified as a number or a time-based phrase.
+#' @param include_endpoints Logical. Whether or not to keep the last value when `length_out` is
+#'  a time-based phrase.
 #' @param skip_values A sequence to skip
 #' @param insert_values A sequence to insert
 #'
@@ -27,7 +29,7 @@
 #' - When both `by` and `length_out` are missing, guesses either second or day sequences
 #' - Can skip and insert values if needed.
 #'
-#' __Daily Sequences__
+#' __By: Daily Sequences__
 #'
 #' Make a daily sequence with `tk_make_timeseries(by)`. Examples:
 #'
@@ -35,13 +37,29 @@
 #'  - Every 2-Weeks: `by = "2 weeks"`
 #'  - Every 6-months: `by = "6 months"`
 #'
-#' __Sub-Daily Sequences__
+#' __By: Sub-Daily Sequences__
 #'
 #' Make a sub-daily sequence with `tk_make_timeseries(by)`. Examples:
 #'
 #' - Every minute: `by = "min"`
 #' - Every 30-seconds: `by = "30 sec"`
 #' - Every 2-hours: `by = "2 hours`
+#'
+#' __Length Out__
+#'
+#' The `length_out` can be specified by number of observation or complex time-based expressions.
+#' The following examples are all possible.
+#'
+#' - `length_out = 12` Creates 12 evenly spaced observations.
+#' - `length_out = "12 months"` Adjusts the end date so it falls on the 12th month.
+#'
+#'
+#' __Include Endpoint__
+#'
+#' Sometimes the last date is not desired.
+#' For example, if the user specifies `length_out = 12 months`, the user may want the last value
+#' to be the 12th month and not the 13th. Just toggle, `include_endpoint = FALSE` to obtain this
+#' behavior.
 #'
 #' __Skip / Insert Timestamps__
 #'
@@ -66,24 +84,43 @@
 #'
 #' # ---- DATE ----
 #'
-#' # Date Sequence - By Day
-#' tk_make_timeseries("2017-01-01", "2017-12-31") # Guesses by = "day"
+#' # Start + End, Guesses by = "day"
+#' tk_make_timeseries("2017-01-01", "2017-12-31")
 #'
-#' # Date Sequence - By Day
+#' # Start + Length Out, Guesses by = "day"
 #' tk_make_timeseries("2012-01-01", length_out = 6) # Guesses by = "day"
 #'
-#' # Date Sequence - By Month
-#' tk_make_timeseries("2012-01-01", by = "1 month", length_out = 6) # Switch to month
+#' # Start + By + Length Out, Spacing 6 observations by monthly interval
+#' tk_make_timeseries("2012-01-01", by = "1 month", length_out = 6)
 #'
+#' # Start + By + Length Out, Phrase "1 year 6 months"
+#' tk_make_timeseries("2012-01-01", by = "1 month",
+#'                    length_out = "1 year 6 months", include_endpoints = FALSE)
+#'
+#' # Going in Reverse, End + Length Out
+#' tk_make_timeseries(end_date = "2012-01-01", by = "1 month",
+#'                    length_out = "1 year 6 months", include_endpoints = FALSE)
 #'
 #' # ---- DATE-TIME ----
 #'
-#' # Date-Time Sequence - By Second
-#' tk_make_timeseries("2016-01-01 01:01:02", "2016-01-01 01:01:04") # Guesses by second
+#' # Start + End, Guesses by second
+#' tk_make_timeseries("2016-01-01 01:01:02", "2016-01-01 01:01:04")
 #'
 #' # Date-Time Sequence - By 10 Minutes
 #' # - Converts to date-time automatically & applies 10-min interval
 #' tk_make_timeseries("2017-01-01", "2017-01-02", by = "10 min")
+#'
+#'
+#' # --- REMOVE / INCLUDE ENDPOINTS ----
+#'
+#' # Last value in this case is desired
+#' tk_make_timeseries("2017-01-01", by = "30 min", length_out = "6 hours")
+#'
+#' # Last value in monthly case is not wanted
+#' tk_make_timeseries("2012-01-01", by = "1 month",
+#'                    length_out = "12 months",
+#'                    include_endpoints = FALSE) # Removes unnecessary last value
+#'
 #'
 #' # ---- SKIP & INSERT VALUES ----
 #'
@@ -101,6 +138,7 @@ NULL
 #' @rdname tk_make_timeseries
 #' @export
 tk_make_timeseries <- function(start_date, end_date, by, length_out = NULL,
+                               include_endpoints = TRUE,
                                skip_values = NULL, insert_values = NULL) {
 
     # Condition count for everything except by. If by is missing, will be guessed.
@@ -157,7 +195,7 @@ tk_make_timeseries <- function(start_date, end_date, by, length_out = NULL,
 
     # Apply parser
     if (parser == "datetime") {
-        # Sub-daily
+        # Sub-daily ----
 
         if (!rlang::is_missing(start_date)) {
             tryCatch({
@@ -183,6 +221,27 @@ tk_make_timeseries <- function(start_date, end_date, by, length_out = NULL,
             }
         }
 
+        # Handle character length_out
+        drop_end_1   <- FALSE
+        drop_begin_1 <- FALSE
+        if (!rlang::is_missing(length_out)) {
+            if (is.character(length_out)) {
+                if (!rlang::is_missing(start_date)) {
+                    end_date <- start_date %+time% length_out
+                    length_out <- NULL
+                    drop_end_1 <- TRUE
+                    if (is.na(end_date)) rlang::abort("length_out returned an invalid end_date, and may not exist. Try a new length_out.")
+                } else if (!rlang::is_missing(end_date)) {
+                    start_date   <- end_date %-time% length_out
+                    length_out   <- NULL
+                    drop_begin_1 <- TRUE
+                    if (is.na(start_date)) rlang::abort("length_out returned an invalid start_date, and may not exist. Try a new length_out.")
+                } else {
+                    rlang::abort("Please specify a start_date or end_date")
+                }
+            }
+        }
+
         # Use seq.POSIXt to create the sequence
         seq <- seq.POSIXt(
             from = start_date,
@@ -191,11 +250,28 @@ tk_make_timeseries <- function(start_date, end_date, by, length_out = NULL,
             length.out = length_out
         )
 
+        # Drop last value if length_out is character
+        # - This happens because the end_date is 1 period longer than the desired length out
+        if (!rlang::is_missing(length_out)) {
+            if (is.character(length_out)) {
+                seq <- seq[1:(length(seq)-1)]
+            }
+        }
+
+        # Drop last value if length_out is character
+        # - This happens because the end_date is 1 period longer than the desired length out
+        if (drop_begin_1 && !include_endpoints) {
+            seq <- seq[-1]
+        }
+        if (drop_end_1 && !include_endpoints) {
+            seq <- seq[-length(seq)]
+        }
+
         # Remove skip / insert values
         seq <- add_subtract_sequence(seq, skip_values, insert_values)
 
     } else {
-        # Daily
+        # Daily ----
 
         if (!rlang::is_missing(start_date)) {
             tryCatch({
@@ -219,12 +295,51 @@ tk_make_timeseries <- function(start_date, end_date, by, length_out = NULL,
             }
         }
 
+        # Handle character length_out
+        drop_end_1   <- FALSE
+        drop_begin_1 <- FALSE
+        if (!rlang::is_missing(length_out)) {
+            if (is.character(length_out)) {
+                if (!rlang::is_missing(start_date)) {
+                    end_date <- start_date %+time% length_out
+                    length_out <- NULL
+                    drop_end_1 <- TRUE
+                    if (is.na(end_date)) rlang::abort("length_out returned an invalid end_date, and may not exist. Try a new length_out.")
+                    if (inherits(end_date, "datetime")) {
+                        warning("Check your length_out. Expecting only date information. Removing time information (hours, mintues, seconds).")
+                        start_date <- lubridate::as_date(end_date)
+                    }
+
+                } else if (!rlang::is_missing(end_date)) {
+                    start_date <- end_date %-time% length_out
+                    length_out <- NULL
+                    drop_begin_1 <- TRUE
+                    if (is.na(start_date)) rlang::abort("length_out returned an invalid start_date, and may not exist. Try a new length_out.")
+                    if (inherits(start_date, "datetime")) {
+                        warning("Check your length_out. Expecting only date information. Removing time information (hours, mintues, seconds).")
+                        start_date <- lubridate::as_date(start_date)
+                    }
+                } else {
+                    rlang::abort("Please specify a start_date or end_date")
+                }
+            }
+        }
+
         seq <- seq.Date(
             from = start_date,
             to   = end_date,
             by   = by,
             length.out = length_out
         )
+
+        # Drop last value if length_out is character
+        # - This happens because the end_date is 1 period longer than the desired length out
+        if (drop_begin_1 && !include_endpoints) {
+            seq <- seq[-1]
+        }
+        if (drop_end_1 && !include_endpoints) {
+            seq <- seq[-length(seq)]
+        }
 
         seq <- add_subtract_sequence(seq, skip_values, insert_values)
 
