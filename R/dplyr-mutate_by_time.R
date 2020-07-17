60a9bd17d286ead8784b99a201376541937fe864
Matt Dancho <mdancho@gmail.com>
2020-07-17 10:25:13 -0400

    mutate_by_time: initial commit

--- R/dplyr-mutate_by_time.R
+++ R/dplyr-mutate_by_time.R
@@ -1,34 +1,24 @@
-#' Summarise (for Time Series Data)
+#' Mutate (for Time Series Data)
 #'
 #' @description
-#' `summarise_by_time()` is a time-based variant of the popular `dplyr::summarise()` function
+#' `mutate_by_time()` is a time-based variant of the popular `dplyr::mutate()` function
 #' that uses `.date_var` to specify a date or date-time column and `.by` to group the
 #' calculation by groups like "5 seconds", "week", or "3 months".
-#'
-#' `summarise_by_time()` and `summarize_by_time()` are synonyms.
-#'
-#' @section Useful summary functions:
-#'
-#' * Sum: [sum()]
-#' * Center: [mean()], [median()]
-#' * Spread: [sd()], [var()]
-#' * Range: [min()], [max()]
-#' * Count: `dplyr::n()`, `dplyr::n_distinct()`
-#' * Position: `dplyr::first()`, `dplyr::last()`, `dplyr::nth()`
-#' * Correlation: [cor()], [cov()]
 #'
 #'
 #' @param .data A `tbl` object or `data.frame`
 #' @param .date_var A column containing date or date-time values to summarize.
 #'  If missing, attempts to auto-detect date column.
-#' @param ... Name-value pairs of summary functions.
-#'   The name will be the name of the variable in the result.
+#' @param ... Name-value pairs.
+#'   The name gives the name of the column in the output.
 #'
 #'   The value can be:
 #'
-#'   * A vector of length 1, e.g. `min(x)`, `n()`, or `sum(is.na(y))`.
-#'   * A vector of length `n`, e.g. `quantile()`.
-#'   * A data frame, to add multiple columns from a single expression.
+#'   * A vector of length 1, which will be recycled to the correct length.
+#'   * A vector the same length as the current group (or the whole data frame
+#'     if ungrouped).
+#'   * `NULL`, to remove the column.
+#'   * A data frame or tibble, to create multiple columns in the output.
 #' @param .by A time unit to summarise by.
 #'   Time units are collapsed using `lubridate::floor_date()` or `lubridate::ceiling_date()`.
 #'
@@ -59,6 +49,7 @@
 #' Time-Based dplyr functions:
 #'
 #' - [summarise_by_time()] - Easily summarise using a date column.
+#' - [mutate_by_time()] - Easily mutate using a date column.
 #' - [filter_by_time()] - Quickly filter using date ranges.
 #' - [between_time()] - Range detection for date or date-time sequences.
 #' - [pad_by_time()] - Insert time series rows with regularly spaced timestamps
@@ -68,54 +59,41 @@
 #' # Libraries
 #' library(timetk)
 #' library(dplyr)
+#' library(tidyr)
 #'
 #' # First value in each month
-#' m4_daily %>%
+#' m4_daily_first_by_month_tbl <- m4_daily %>%
 #'     group_by(id) %>%
-#'     summarise_by_time(
+#'     mutate_by_time(
 #'         .date_var = date,
 #'         .by       = "month", # Setup for monthly aggregation
-#'         # Summarization
-#'         value  = first(value)
+#'         # mutate recycles a single value
+#'         first_value  = first(value)
 #'     )
+#'  m4_daily_first_by_month_tbl
 #'
-#' # Last value in each month (day is first day of next month with ceiling option)
-#' m4_daily %>%
-#'     group_by(id) %>%
-#'     summarise_by_time(
-#'         .by        = "month",
-#'         value      = last(value),
-#'         .type      = "ceiling"
-#'     ) %>%
-#'     # Shift to the last day of the month
-#'     mutate(date = date %-time% "1 day")
-#'
-#' # Total each year (.by is set to "year" now)
-#' m4_daily %>%
-#'     group_by(id) %>%
-#'     summarise_by_time(
-#'         .by        = "year",
-#'         value      = sum(value)
-#'     )
-#'
+#'  # Visualize Time Series vs 1st Value Each Month
+#'  m4_daily_first_by_month_tbl %>%
+#'      pivot_longer(value:first_value) %>%
+#'      plot_time_series(date, value, name,
+#'                       .facet_scale = "free", .facet_ncol = 2,
+#'                       .smooth = F, .interactive = F)
 #'
 #' @export
-summarise_by_time <- function(.data, .date_var, .by = "day", ...,
+mutate_by_time <- function(.data, .date_var, .by = "day", ...,
                               .type = c("floor", "ceiling", "round")) {
 
     if (rlang::quo_is_missing(rlang::enquo(.date_var))) {
         message(".date_var is missing. Using: ", tk_get_timeseries_variables(.data)[1])
     }
 
-    UseMethod("summarise_by_time")
+    UseMethod("mutate_by_time")
 }
 
-#' @rdname summarise_by_time
-#' @export
-summarize_by_time <- summarise_by_time
+
 
 #' @export
-summarise_by_time.default <- function(.data, .date_var, .by = "day", ...,
+mutate_by_time.default <- function(.data, .date_var, .by = "day", ...,
                                       .type = c("floor", "ceiling", "round")) {
 
     stop("Object is not of class `data.frame`.", call. = FALSE)
@@ -123,7 +101,7 @@ summarise_by_time.default <- function(.data, .date_var, .by = "day", ...,
 }
 
 #' @export
-summarise_by_time.data.frame <- function(.data, .date_var, .by = "day", ...,
+mutate_by_time.data.frame <- function(.data, .date_var, .by = "day", ...,
                                          .type = c("floor", "ceiling", "round")) {
 
     data_groups_expr   <- rlang::syms(dplyr::group_vars(.data))
@@ -145,12 +123,15 @@ summarise_by_time.data.frame <- function(.data, .date_var, .by = "day", ...,
         .f <- lubridate::round_date
     }
 
-    # Time-based summarization logic
+    # Time-based mutate logic
     ret_tbl <- .data %>%
-        dplyr::mutate(!! date_var_expr := .f(!! date_var_expr, unit = .by)) %>%
-        dplyr::group_by_at(.vars = dplyr::vars(!!! data_groups_expr, !! date_var_expr)) %>%
+        dplyr::mutate(.date_var_collapsed := .f(!! date_var_expr, unit = .by)) %>%
+        dplyr::group_by_at(.vars = dplyr::vars(!!! data_groups_expr, .date_var_collapsed)) %>%
         dplyr::arrange(!! date_var_expr, .by_group = TRUE) %>%
-        dplyr::summarize(...)
+        dplyr::mutate(...) %>%
+        dplyr::ungroup() %>%
+        dplyr::select(-.date_var_collapsed) %>%
+        dplyr::group_by_at(.vars = dplyr::vars(!!! data_groups_expr))
 
     return(ret_tbl)
 
