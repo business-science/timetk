868a6fbb3a271c21ba17593f624cc2e50b79a67c
Matt Dancho <mdancho@gmail.com>
2022-01-18 20:30:20 -0500

    fix pkgdown

--- R/timetk-package.R
+++ R/timetk-package.R
@@ -1,20 +1,17 @@
-#' timetk: a toolkit for time series
+#' timetk: Time Series Analysis in the Tidyverse
 #'
 #' The `timetk` package combines a collection of coercion tools for
 #' time series analysis.
 #'
-#'
-#'
 #' @details
 #' The `timetk` package has several benefits:
 #'
-#' 1. Index extraction: get the time series index from any time series object.
-#' 2. Understand time series: create a signature and summary from a time series index.
-#' 3. Build future time series: create a future time series from an index.
-#' 4. Coerce between time-based tibbles (tbl) and the major time series data types xts, zoo, zooreg, and ts: Simplifies coercion and maximizes time-based data retention during coercion to regularized time series (e.g. ts).
+#' 1. Visualizing Time Series
+#' 2. Wrangling Time Series.
+#' 3. Preprocessing and Feature Engineering.
 #'
-#' To learn more about `timetk`, start with the vignettes:
-#' `browseVignettes(package = "timetk")`
+#' To learn more about `timetk`, start with the documentation:
+#' [https://business-science.github.io/timetk/](https://business-science.github.io/timetk/)
 #'
 #' @docType package
 #' @name timetk
