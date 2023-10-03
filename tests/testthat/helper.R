300cf8838cbda008159601302bb4c2c844f12a80
olivroy <olivierroy71@hotmail.com>
2023-10-03 11:37:44 -0400

    tz warning only present on R 4.3 and R devel.

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,6 +1,11 @@
 expect_tz_warning <- function(object) {
     # FIXME when the tzone warning is fixed, delete this function,
     # use Replace all expect_tz_warning -> expect_no_warning
+    # This is for R >= 4.3.0
     # Issue #156
-    expect_warning(object, regexp = "tzone")
+    if (getRversion() >= "4.3.0") {
+        expect_warning(object, regexp = "tzone")
+    } else {
+        expect_no_warning(object)
+    }
 }
